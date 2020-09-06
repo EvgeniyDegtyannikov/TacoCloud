a944172083d013351e6847788cd7ae4cec165d02
EvgeniyDegtyannikov <EvgeniyDegtyannikov@yandex.ru>
2020-09-06 11:34:15 +0400

    Third chapter - jdbc

--- src/main/java/tacos/web/controllers/DesignTacoController.java
+++ src/main/java/tacos/web/controllers/DesignTacoController.java
@@ -1,61 +1,70 @@
 package tacos.web.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
 import tacos.domain.Ingredient;
+import tacos.domain.Order;
 import tacos.domain.Taco;
 
 import javax.validation.Valid;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
 @RequestMapping(path = "/design")
+@SessionAttributes("order")
 public class DesignTacoController {
+    private final IngredientRepository ingredientRepository;
+    private final TacoRepository tacoRepository;
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepository, TacoRepository tacoRepository) {
+        this.ingredientRepository = ingredientRepository;
+        this.tacoRepository = tacoRepository;
+    }
 
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
-        );
-        Ingredient.Type[] types = Ingredient.Type.values();
-        for (Ingredient.Type type :
-                types) {
-            model.addAttribute(type.toString().toLowerCase(), ingredients.stream()
-                    .filter(i -> i.getType().equals(type))
+        List<Ingredient> ingredientList = new ArrayList<>();
+        ingredientRepository.findAll().forEach(ingredientList::add);
+        for (Ingredient.Type type : Ingredient.Type.values()
+        ) {
+            model.addAttribute(type.toString().toLowerCase(), ingredientList.stream()
+                    .filter(t -> t.getType().equals(type))
                     .collect(Collectors.toList())
             );
         }
     }
 
+    @ModelAttribute(name = "order")
+    public Order order() {
+        return new Order();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
+
     @GetMapping
-    public String showDesignForm(Model model) {
-        model.addAttribute("design", new Taco());
+    public String showDesignForm() {
         return "design";
     }
 
     @PostMapping
-    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors) {
+    public String processDesign(@Valid @ModelAttribute("taco") Taco taco, Errors errors, @ModelAttribute("order") Order order) {
         if (errors.hasErrors())
             return "design";
-        log.info("Processing design: " + design);
+        Taco saved = tacoRepository.save(taco);
+        order.addDesign(saved);
         return "redirect:/orders/current";
     }
 }
--- src/main/java/tacos/web/controllers/OrderController.java
+++ src/main/java/tacos/web/controllers/OrderController.java
@@ -1,12 +1,12 @@
 package tacos.web.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import tacos.data.OrderRepository;
 import tacos.domain.Order;
 
 import javax.validation.Valid;
@@ -14,19 +14,27 @@ import javax.validation.Valid;
 @Slf4j
 @Controller
 @RequestMapping(path = "/orders")
+@SessionAttributes("order")
 public class OrderController {
 
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
     @GetMapping(path = "/current")
-    public String orderForm(Model model) {
-        model.addAttribute("order", new Order());
+    public String orderForm() {
         return "orderForm";
     }
 
     @PostMapping(path = "/current")
-    public String processOrder(@Valid Order order, Errors errors) {
+    public String processOrder(@Valid @ModelAttribute("order") Order order, Errors errors, SessionStatus sessionStatus) {
         if (errors.hasErrors())
             return "orderForm";
-        log.info("Order submitted: " + order);
+        orderRepository.save(order);
+        sessionStatus.setComplete();
         return "redirect:/";
     }
 }
